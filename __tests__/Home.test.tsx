12d2445d595cee2fa03c9332265f0e4a1dbdc94d
Ashfaq <ashfaqkabir@gmail.com>
2023-11-26 20:52:12 +0600

    Removed failed Test case to test CI workflow

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -7,6 +7,6 @@ it("Should Render Home Component and 'Learn' text in it", () => {
     // AAA Method of Unit Testing
     render(<Home/>) // Arrange ( Arrange or Call the Component u wanna test )
 
-    const learnText = screen.getByText("Learnsdfkjsdf") // Act ( Get or Click whatever behaviour u want it to do )
+    const learnText = screen.getByText("Learn") // Act ( Get or Click whatever behaviour u want it to do )
     expect(learnText).toBeInTheDocument() // Assert ( Assert what u r expecting the function to do )
 })
