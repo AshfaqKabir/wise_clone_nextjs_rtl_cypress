7745e5a23a29b1579d2e72d4c3fde0d2030ad9f2
AshfaqKabir <ashfaqkabir@gmail.com>
2023-11-29 11:48:16 +0000

    Added NextUI

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,12 +1,12 @@
-import {render, screen} from "@testing-library/react"
-import Home from "../app/page"
+import { render, screen } from "@testing-library/react";
+import Home from "../app/page";
 
-const {expect} = require("@jest/globals")
+const { expect } = require("@jest/globals");
 
-it("Should Render Home Component and 'Learn' text in it", () => {
-    // AAA Method of Unit Testing
-    render(<Home/>) // Arrange ( Arrange or Call the Component u wanna test )
+it("Should Render Home Component and 'Hello' text in it", () => {
+  // AAA Method of Unit Testing
+  render(<Home />); // Arrange ( Arrange or Call the Component u wanna test )
 
-    const learnText = screen.getByText("Learn") // Act ( Get or Click whatever behaviour u want it to do )
-    expect(learnText).toBeInTheDocument() // Assert ( Assert what u r expecting the function to do )
-})
+  const learnText = screen.getByText("Hello"); // Act ( Get or Click whatever behaviour u want it to do )
+  expect(learnText).toBeInTheDocument(); // Assert ( Assert what u r expecting the function to do )
+});
